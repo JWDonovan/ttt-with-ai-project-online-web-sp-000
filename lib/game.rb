1f4d8ee2a6ac90386b11e016c72bbb9f23f2668f
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-16 21:03:57 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,5 @@
 class Game
   attr_accessor :board, :player_1, :player_2
-
   WIN_COMBINATIONS = [
     [0,1,2],
     [3,4,5],
@@ -12,30 +11,18 @@ class Game
     [6,4,2]
   ]
 
-  def initialize(player_1 = Player::Human.new("X"), player_2 = Player::Human.new("0"), board = Board.new)
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
+    @board = board
     @player_1 = player_1
     @player_2 = player_2
-    @board = board
+  end
+
+  def over?
+    won? || draw?
   end
 
   def current_player
     @board.turn_count % 2 == 0 ? @player_1 : @player_2
-  end
-
-  def won?
-    WIN_COMBINATIONS.detect do |combo|
-      @board.cells[combo[0]] == @board.cells[combo[1]] &&
-      @board.cells[combo[1]] == @board.cells[combo[2]] &&
-      @board.taken?(combo[0] + 1)
-    end
-  end
-
-  def draw?
-    @board.full? && !won?
-  end
-
-  def over?
-    draw? || won?
   end
 
   def winner
@@ -46,16 +33,14 @@ class Game
 
   def turn
     player = current_player
-    move = player.move(@board)
-
-    if !@board.valid_move?(move)
+    current_move = player.move(@board)
+    if !@board.valid_move?(current_move)
       turn
     else
-      puts "Turn: #{@board.turn_count + 1}"
+      puts "Turn: #{@board.turn_count+1}\n"
       @board.display
-      @board.update(move, player)
-
-      puts "#{player.token} moved #{move}"
+      @board.update(current_move, player)
+      puts "#{player.token} moved #{current_move}"
       @board.display
       puts "\n\n"
     end
@@ -65,11 +50,22 @@ class Game
     while !over?
       turn
     end
-
     if won?
       puts "Congratulations #{winner}!"
     elsif draw?
       puts "Cat's Game!"
     end
+  end
+
+  def won?
+    WIN_COMBINATIONS.detect do |combo|
+      @board.cells[combo[0]] == @board.cells[combo[1]] &&
+      @board.cells[combo[1]] == @board.cells[combo[2]] &&
+      @board.taken?(combo[0]+1)
+    end
+  end
+
+  def draw?
+    @board.full? && !won?
   end
 end
