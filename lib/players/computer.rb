9d3e3c105e2e87b47e9503658d951e1285c0cf81
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-16 21:17:00 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -8,7 +8,9 @@ module Players
       elsif board.turn_count == 1
         move = "1"
       elsif board.turn_count == 2
-        move = [1, 3, 7, 9].detect{|i| !board.taken?(i)}.to_s
+        move = [1, 3, 7, 9].detect do |index|
+          !board.taken?(i)
+        end.to_s
       elsif board.turn_count == 3 && (board.position(1) == board.position(9) || board.position(3) == board.position(7))
         move = "2"
       else
