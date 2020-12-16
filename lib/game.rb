09977f590317c918e9a38fceb4ec97767fa2d8e3
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-16 20:49:10 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -60,4 +60,16 @@ class Game
       puts "\n\n"
     end
   end
+
+  def play
+    while !over?
+      turn
+    end
+
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cat's Game!"
+    end
+  end
 end
